9d94a6b10615cc7148274bd771beb00afcc7a5b2
beejh02 <joon0448@naver.com>
2025-08-02 21:32:56 +0900

    .

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return <div className="App">
+    <h1 style={{
+      color: "#f0f",
+      backgroundColor: "#green",
+      }}
+    >어
+    </h1>
+  </div>
+}
+
+export default App;
