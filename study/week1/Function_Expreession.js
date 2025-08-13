bbc38005470aca0b39807a80e76c05cbddee0734
joon0448@beejh02 <joon0448@naver.com>
2025-08-13 15:31:08 +0900

    upload 함수 표현식(익명&기명)

---
+++ study/week1/Function_Expreession.js
@@ -0,0 +1,12 @@
+//익명 함수 포함식
+const add = function(a, b) {
+    return a + b;
+};
+
+// 기명 함수 표현식
+const multiply = function mul(a, b) {
+    return a * b;
+};
+
+console.log(add(1, 2));
+console.log(multiply(2, 3));
